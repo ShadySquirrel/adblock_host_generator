c435ffad038317c90a01485a27bc21d71735dd53
I <ShadySquirrel@users.noreply.github.com>
2017-08-05 13:29:51 +0200

    generator: git_config: fix description

--- git_config.example.py
+++ git_config.example.py
@@ -1,7 +1,7 @@
 '''
 This is an example configuration.
 
-Please, make a copy of this file, rename it to "git_config",
+Please, make a copy of this file, rename it to "git_config.py",
 and put your credential data inside. File is added to .gitignore so it won't be pushed anywhere.
 '''
 git_config = {
